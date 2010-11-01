15a3a1e26964e48783a7468efa0426ea17828d68
George <karagog@gmail.com>
2010-11-01 17:35:47 -0400

    Adding new test cases

--- Custom/Tests/GVariant/tst_gvarianttest.cpp
+++ Custom/Tests/GVariant/tst_gvarianttest.cpp
@@ -14,6 +14,8 @@ public:
 
 private Q_SLOTS:
     void test_basic_types();
+    void test_simple_qt_types();
+    void test_collections();
 };
 
 GVariantTest::GVariantTest()
@@ -41,6 +43,55 @@ void GVariantTest::test_basic_types()
     qDebug(gv1.ToXmlString().c_str());
     qDebug(gv2.ToXmlString().c_str());
     QVERIFY(gv1 != gv2);
+
+    gv2.FromXmlQString(gv1.ToXmlQString());
+    QVERIFY(gv1 == gv2);
+
+    // Char
+    gv1 = QChar(0x0);
+    gv2.FromXmlQString(gv1.ToXmlQString());
+    qDebug(gv1.ToXmlString().c_str());
+    QVERIFY(gv1 == gv2);
+    QVERIFY(gv2 == QChar(0x0));
+
+    // Bool
+    gv1 = true;
+    gv2.FromXmlQString(gv1.ToXmlQString());
+    QVERIFY(gv1 == gv2);
+    QVERIFY(gv2 == true);
+
+    qDebug(gv1.ToXmlString().c_str());
+
+    gv1 = false;
+    gv2.FromXmlQString(gv1.ToXmlQString());
+    QVERIFY(gv1 == gv2);
+    QVERIFY(gv2 == false);
+
+    // Double
+    gv1 = (double)5.68921;
+    gv2.FromXmlQString(gv1.ToXmlQString());
+    QVERIFY(gv1 == gv2);
+    QVERIFY(gv2 == 5.68921);
+
+    qDebug(gv1.ToXmlString().c_str());
+
+    // Float
+    gv1 = (float)5.68921;
+    qDebug(gv1.ToXmlString().c_str());
+    gv2.FromXmlQString(gv1.ToXmlQString());
+    QVERIFY(gv1 == gv2);
+    QVERIFY(gv2 == 5.68921);
+
+    qDebug(gv1.ToXmlString().c_str());
+}
+
+void GVariantTest::test_simple_qt_types()
+{
+
+}
+
+void GVariantTest::test_collections()
+{
 
 }
 
