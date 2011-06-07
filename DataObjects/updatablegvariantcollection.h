e92d5f270caad84e500673a341dd451f5c454489
George Karagoulis <karagog@gmail.com>
2011-06-07 21:44:43 +0200

    Updated copyright dates to 2011

--- DataObjects/updatablegvariantcollection.h
+++ DataObjects/updatablegvariantcollection.h
@@ -1,4 +1,4 @@
-/*Copyright Copyright 2011 George Karagoulis
+/*Copyright 2011 George Karagoulis
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
