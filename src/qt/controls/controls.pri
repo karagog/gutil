9115d74a6e2b212a9ae1dc368a99fdfdcae64bd8
George Karagoulis <karagog@gmail.com>
2014-08-31 13:56:22 -0400

    Revert "Removed the progressbar control because Qt already has QProgressDialog"
    This control will actually be useful
    
    This reverts commit cab04806084f82ec79340fdb8483a07c118aaa37.
    
    Conflicts:
    	src/qt/controls/controls.pri

--- src/qt/controls/controls.pri
+++ src/qt/controls/controls.pri
@@ -1,9 +1,11 @@
 
 HEADERS += \
+    controls/progressbarcontrol.h \
     controls/lineeditwithbutton.h \
     controls/stopwatch.h \
     controls/selectdaterange.h
 SOURCES += \
+    controls/progressbarcontrol.cpp \
     controls/lineeditwithbutton.cpp \
     controls/stopwatch.cpp \
     controls/selectdaterange.cpp
---
+++ src/qt/controls/progressbarcontrol.cpp
@@ -0,0 +1,61 @@
+/*Copyright 2010-2013 George Karagoulis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+#include "progressbarcontrol.h"
+#include "gutil_macros.h"
+#include <QStackedLayout>
+#include <QVBoxLayout>
+
+NAMESPACE_GUTIL1(QT);
+
+
+ProgressBarControl::ProgressBarControl(QWidget *parent)
+    :QWidget(parent)
+{
+    _button.setStyleSheet("background-color: rgba(0,0,0,0);");
+
+    _progressBar.setTextVisible(false);
+
+    setLayout(new QVBoxLayout(this));
+    layout()->setContentsMargins(0, 0, 0, 0);
+    layout()->addWidget(&_progressBar);
+
+    _label.setAlignment(Qt::AlignCenter);
+    QFont f;  f.setBold(true);
+    _label.setFont(f);
+
+    _progressBar.setLayout(new QStackedLayout(&_progressBar));
+    _progressBar.layout()->setContentsMargins(0, 0, 0, 0);
+    ((QStackedLayout*)_progressBar.layout())->setStackingMode(QStackedLayout::StackAll);
+    _progressBar.layout()->addWidget(&_label);
+
+    _button.hide();
+    _progressBar.layout()->addWidget(&_button);
+}
+
+void ProgressBarControl::setButtonEnabled(bool which)
+{
+    if(which)
+    {
+        _button.show();
+        ((QStackedLayout *)_progressBar.layout())->setCurrentWidget(&_button);
+    }
+    else
+    {
+        _button.hide();
+    }
+}
+
+
+END_NAMESPACE_GUTIL1;
