ffafa7c672b521f897e42dc81d3a3861a9946d6f
George Karagoulis <karagog@gmail.com>
2014-11-01 12:46:42 -0400

    Updated the license dates for 2015. I know it's early but I didn't want to do it again in the next few months.

--- src/core/data_objects/id.h
+++ src/core/data_objects/id.h
@@ -1,4 +1,4 @@
-/*Copyright 2012 George Karagoulis
+/*Copyright 2012-2015 George Karagoulis
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
